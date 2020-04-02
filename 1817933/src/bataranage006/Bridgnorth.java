e072bd4b04acb2559ac5915354ad3271cd5f3d05
Lakshitha <lakshthilo9094@gmail.com>
2020-04-02 17:16:56 +0100

    initial

---
+++ 1817933/src/bataranage006/Bridgnorth.java
@@ -0,0 +1,14 @@
+package bataranage006;
+public class Bridgnorth {
+  static transient String[] coati = {
+    "Where there is a lack of honor in government, the morals of the whole people are poisoned.","Herbert Hoover","",
+    "The least initial deviation from the truth is multiplied later a thousandfold.","Aristotle","",
+    "Everyone is a moon and has a dark side which he never shows to anybody.","Mark Twain","",
+    "The right thing to do never requires any subterfuge, it is always simple and direct.","Calvin Coolidge","",
+    "The victor will never be asked if he told the truth.","Adolf Hitler","",
+    "Swallow your pride occasionally, it's not fattening.","Frank Tyger","",
+    "History is more or less bunk.","Henry Ford","",
+    "The most incomprehensible thing about the world is that it is at all comprehensible.","Albert Einstein","",
+    "I'd rather hear an old truth than a new lie.","Chris Bowyer","",
+    "It is noble to be good; it is still nobler to teach others to be good - and less trouble.","Mark Twain",""};
+}
---
+++ 1817933/src/bataranage006/IOSpecialist.java
@@ -0,0 +1,14 @@
+package bataranage006;
+
+/**
+ * @author Kevan Buckley, maintained by Lakshitha Bataranage
+ * @version 20.01, 2020
+ */
+
+public class IOSpecialist {
+  public IOSpecialist() {   
+  }
+  public String getString(){
+    return IOLibrary.getString();
+  }
+}
